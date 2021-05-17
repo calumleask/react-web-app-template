0406215d039240c0dc922608732f77fd38064fdd
calumleask <cjleask@hotmail.co.uk>
2021-05-18 00:43:49 +0100

    Mocking scss files in tests

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^~/(.*)$": "<rootDir>/src/$1",
+    "\\.(css|scss)$": "<rootDir>/src/__mocks__/styleMock.js"
+  }
+
 };
